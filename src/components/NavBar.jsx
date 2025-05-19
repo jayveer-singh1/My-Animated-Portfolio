21868346e1bdc91846c3145e19add1666033b017
Jayveer Singh <149516764+jayveer-singh1@users.noreply.github.com>
2025-05-19 13:08:29 +0530

    Update NavBar.jsx
    Name update to Jayveer Singh

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
     <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
       <div className="inner">
         <a href="#hero" className="logo">
-          Jayveer Singh Rawat
+          Jayveer Singh
         </a>
 
         <nav className="desktop">
